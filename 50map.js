2727d9df2178c2940e8eddce933b6d54e90f4d6c
yanhan <yanhan@hdkjrd.com>
2021-06-15 17:39:22 +0800

    .

--- 50map.js
+++ 50map.js
@@ -64,3 +64,69 @@ var isValid = function (s) {
     }
     return stack.length === 0;
 }
+
+/**
+ * 两数之和
+ */
+var twoSum = function (nums, target) {
+    const map = new Map();
+    for (let i = 0; i < nums.length; i++) {
+        const n = nums[i];
+        const n2 = target - n;
+        if (map.has(n2)) {
+            return [map.get(n2), i];
+        } else {
+            map.set(n, i);
+        }
+    }
+}
+
+/**
+ * 无重复字符的最长子串
+ */
+var lengthOfLongestSubstring = function (s) {
+    let l = 0;
+    let res = 0;
+    const map = new Map();
+    for (let r = 0; r < s.length; r++) {
+        if (map.has(s[r]) && map.get(s[r]) >= 1) {
+            l = map.get(s[r]) + 1;
+        }
+        res = Math.max(res, r - l + 1);
+        map.set(s[r], r);
+    }
+    return res;
+}
+
+/**
+ * 最小覆盖子串
+ */
+var minWindow = function (s, t) {
+    let l = 0;
+    let r=0;
+    const need = new Map();
+    for (let c of t){
+        need.set(c, need.has(c) ? need.get(c) + 1 : 1)
+    }
+    let needType = need.size;
+    let res = '';
+    while (r < s.length){
+        const c = s[r];
+        if(need.has(c)){
+            need.set(c, need.get(c) - 1);
+            if(need.get(c) === 0) needType -= 1;
+        }
+        while (needType === 0){
+            const newRes = s.substring(l, r + 1);
+            if(!res || newRes.length < res.length) res = newRes;
+            const c2 = s[l];
+            if(need.has(c2)){
+                need.set(c2, need.get(c2) + 1);
+                if(need.get(c2) === 1) needType += 1;
+            }
+            l += 1;
+        }
+        r += 1;
+    }
+    return res;
+}
--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # js-ds-algorithm
 
 参考资料：<br>
-《JavaScript版数据结构与算法》，慕课网lewis老师，https://www.youtube.com/playlist?list=PLwIrqQCQ5pQmjH6YyFvH2A9FYL6bBB4Ra
+《JavaScript版数据结构与算法》，慕课网lewis老师<br>
+https://www.youtube.com/playlist?list=PLwIrqQCQ5pQmjH6YyFvH2A9FYL6bBB4Ra
