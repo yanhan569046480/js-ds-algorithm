ddd206692e0597414b28870b4175ce382e44ef22
yanhan <yanhan@hdkjrd.com>
2021-06-23 11:15:49 +0800

    .

--- 30linkedList.js
+++ 30linkedList.js
@@ -55,7 +55,7 @@ var reverseList = function (head) {
     let p1 = head;
     let p2 = null;
     while (p1) {
-        const temp = p1.netx;
+        const temp = p1.next;
         p1.next = p2;
         p2 = p1;
         p1 = temp;
